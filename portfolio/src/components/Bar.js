4c9628929e9295632449c70d2512c991a2ed4b39
15MPa <m.pajda@gmail.com>
2020-08-10 20:14:22 +0200

    RWD added

--- portfolio/src/components/Bar.js
+++ portfolio/src/components/Bar.js
@@ -7,8 +7,17 @@ const Bar = ({choice}) => {
     const [login,setLogin] = useState(false);
     const [register, setRegister] = useState(false);
 
+    const hamburger = ()=> {
+        var x = document.getElementById("myLinks");
+        if (x.style.display === "block") {
+          x.style.display = "none";
+        } else {
+          x.style.display = "block";
+        }
+      }
+
     return(
-        
+        <>
         <div className="bars">
             
             
@@ -17,6 +26,7 @@ const Bar = ({choice}) => {
                 <button onClick={(ev)=>{choice('register')}}>Załóż konto</button>
             </div>
             <nav className="nav-bar">
+
                 <ul className="nav-bar-list">
                     <li><Link to={'header'} onClick={(ev)=>choice(false)}>Start</Link></li>
                     <li><Link to={'three-col'}>O co chodzi?</Link></li>
@@ -24,10 +34,24 @@ const Bar = ({choice}) => {
                     <li><Link to={'who-help'}>Fundacja i organizacje</Link></li>
                     <li><Link to={'contact'}>Kontakt</Link></li>
                 </ul>
-            </nav>
-            
-            
+            </nav>    
         </div>
+
+        <div class="topnav">
+            <a href="" class="active"><Link to={'header'} onClick={(ev)=>choice(false)}>Start</Link></a>
+            
+            <div id="myLinks">
+                    <Link to={'three-col'}>O co chodzi?</Link>
+                    <Link to={'about-us'}>O nas</Link>
+                    <Link to={'who-help'}>Fundacja i organizacje</Link>
+                    <Link to={'contact'}>Kontakt</Link>
+            </div>
+            <a href="javascript:void(0);" class="icon" onClick={hamburger}>
+                <i class="fa fa-bars"></i>
+                </a>
+            
+      </div>
+      </>
     )
 }
 
